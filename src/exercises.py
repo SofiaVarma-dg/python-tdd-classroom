702bfd51e814b6d585d6a65b102d93b09a036d81
SofiaVarma <kola632000@gmail.com>
2020-06-29 16:24:47 +0530

    feat: implemented functions

--- src/exercises.py
+++ src/exercises.py
@@ -2,10 +2,18 @@ def reverse_list(input_list):
     """
     Reverses order of elements in list input_list.
     """
-    return None
+    return input_list[::-1]
+
 
 def count_digits(number):
     """
     Return count of digits
     """
-    return None
+    n = int(number)
+    count = 0
+    
+    while n!=0:
+        count += 1
+        n = int(n/10)
+       
+    return count
